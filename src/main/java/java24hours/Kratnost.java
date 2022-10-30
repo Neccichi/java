67dd58e92922b0ace29ad96fd4123432fa7f39c9
Tim Boniuk <timboniuk@gmail.com>
2022-10-30 22:05:01 +0200

    -

--- src/main/java/java24hours/Kratnost.java
+++ src/main/java/java24hours/Kratnost.java
@@ -1,13 +1,31 @@
 package java24hours;
 
+
 class Kratnost{
     public static void main(String[] arguments){
-        int answer;
-       
-        for(answer = 1; answer<=5200; answer++){
-            if(answer % 13 == 0){
-                System.out.println(answer);
-            } 
+        int person = 1;
+        int save[]= new int[400];
+        int icon = 0;            
+         /*   for(answer = 0; answer<=5200; answer++){
+                if(answer % 13 == 0){
+                    save[i] = answer;
+                } 
+            }
+         */
+         while (icon < 400){
+             if( person % 13 ==0){
+                 save[icon] = person;
+                 icon ++;
+             }
+            person++;
+         }
+         
+        System.out.println("------------------------------------------");
+        System.out.println("Task 2:Нужно сохранить эти числа в массив");
+        
+        for(int s= 0; s<= 399; s++){
+            System.out.println(s+"): "+save[s]);
         }
+        
     }
 }
