3e80874a99743e7437a44f85edd73f59f737e805
Tim Boniuk <timboniuk@gmail.com>
2022-11-01 01:28:17 +0200

    test

--- src/main/java/java24hours/WorkWithTerminal/MainFrame.java
+++ src/main/java/java24hours/WorkWithTerminal/MainFrame.java
@@ -2,25 +2,35 @@ package java24hours.WorkWithTerminal;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.*;
 
-public class MainFrame extends JFrame{
+public class MainFrame extends JFrame implements ActionListener{
     public MainFrame(){
         //Нстройка фрейма
         super("Test frame");
         setLookAndFeel();
         setSize(450, 200);
+          
+        //Text field
+        JLabel pageLabel = new JLabel("Your value -", JLabel.RIGHT);
+        JTextField userData =  new JTextField(20);
+        FlowLayout flo2 = new FlowLayout();
+        setLayout(flo2);
+        add(pageLabel);
+        add(userData);
+        String userDaraReceive = userData.getText();//receive text in a field
         
         //Buttons
         FlowLayout flo = new FlowLayout();
         setLayout(flo);
         JButton okButton = new JButton("OK");
-        
         add(okButton);
         
         //EXIT
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);  
     }
+    
     
     private void setLookAndFeel(){
         try{
@@ -34,5 +44,10 @@ public class MainFrame extends JFrame{
     
     public static void main(String[] arguments){
         MainFrame frame = new MainFrame();
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
 }
